87b30d400ef79e0f19e97e828c6bddaf589e95d0
Emir Mujanovic <emujanovic3@etf.unsa.ba>
2020-02-09 14:29:41 +0100

    Napravljene osnovne funkcionalnosti login prozora

--- src/projekat/LoginController.java
+++ src/projekat/LoginController.java
@@ -1,4 +1,50 @@
 package projekat;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    //TODO negdje cuvati login informacije
+    private String korisnickoIme = "emir";
+    private String sifra = "test";
+
+    public TextField korisnickoImeField;
+    public TextField sifraField;
+
+    public LoginController() {
+    }
+
+    public void prijaviSeAction(ActionEvent actionEvent){
+        boolean greska = false;
+
+        if(korisnickoIme.equals(korisnickoImeField.getText())){
+            korisnickoImeField.getStyleClass().removeAll("poljeNeispravno");
+            korisnickoImeField.getStyleClass().add("poljeIspravno");
+        }else{
+            greska = true;
+            korisnickoImeField.getStyleClass().removeAll("poljeIspravno");
+            korisnickoImeField.getStyleClass().add("poljeNeispravno");
+        }
+
+        if(sifra.equals(sifraField.getText())){
+            sifraField.getStyleClass().removeAll("poljeNeispravno");
+            sifraField.getStyleClass().add("poljeIspravno");
+        }else{
+            greska = true;
+            sifraField.getStyleClass().removeAll("poljeIspravno");
+            sifraField.getStyleClass().add("poljeNeispravno");
+        }
+
+        if(!greska){
+            otvoriGlavniProzor();
+        }
+    }
+
+    public void odustaniAction(ActionEvent actionEvent){
+        System.exit(0);
+    }
+
+    private void otvoriGlavniProzor() {
+
+    }
 }
--- src/projekat/Main.java
+++ src/projekat/Main.java
@@ -6,13 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        LoginController ctrl = new LoginController();
+        loader.setController(ctrl);
+        Parent root = loader.load();
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
         primaryStage.show();
     }
 

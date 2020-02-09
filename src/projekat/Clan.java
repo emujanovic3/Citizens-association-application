f648f08d0a684e743c90516083549811e8a3fe4a
Emir Mujanovic <emujanovic3@etf.unsa.ba>
2020-02-09 17:28:24 +0100

    Napravljene klase Prebivaliste i Clan

--- src/projekat/Clan.java
+++ src/projekat/Clan.java
@@ -9,4 +9,61 @@ public class Clan {
     LocalDate datumRodjenja;
     Prebivaliste prebivaliste;
     Drzavljanstvo drzavljanstvo;
+
+    public Clan(int id, String ime, String prezime, LocalDate datumRodjenja, Prebivaliste prebivaliste, Drzavljanstvo drzavljanstvo) {
+        this.id = id;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.datumRodjenja = datumRodjenja;
+        this.prebivaliste = prebivaliste;
+        this.drzavljanstvo = drzavljanstvo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public LocalDate getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(LocalDate datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public Prebivaliste getPrebivaliste() {
+        return prebivaliste;
+    }
+
+    public void setPrebivaliste(Prebivaliste prebivaliste) {
+        this.prebivaliste = prebivaliste;
+    }
+
+    public Drzavljanstvo getDrzavljanstvo() {
+        return drzavljanstvo;
+    }
+
+    public void setDrzavljanstvo(Drzavljanstvo drzavljanstvo) {
+        this.drzavljanstvo = drzavljanstvo;
+    }
 }
--- src/projekat/Prebivaliste.java
+++ src/projekat/Prebivaliste.java
@@ -4,4 +4,34 @@ public class Prebivaliste {
     private String adresa;
     private String grad;
     private String drzava;
+
+    public Prebivaliste(String adresa, String grad, String drzava) {
+        this.adresa = adresa;
+        this.grad = grad;
+        this.drzava = drzava;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public String getGrad() {
+        return grad;
+    }
+
+    public void setGrad(String grad) {
+        this.grad = grad;
+    }
+
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        this.drzava = drzava;
+    }
 }

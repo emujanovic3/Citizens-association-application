d2384f061a7c81013fc11774c035c75cb196db4e
Emir Mujanovic <emujanovic3@etf.unsa.ba>
2020-02-13 00:33:46 +0100

    Napravljen fxml pocetnog prozora

--- src/projekat/Main.java
+++ src/projekat/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
